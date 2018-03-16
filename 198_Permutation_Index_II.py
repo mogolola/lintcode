43737c1923bb2784f9dc15454208b9f97936e8dd
mogolola <1158060815@qq.com>
2018-03-16 16:47:22 +0100

    update

--- 198_Permutation_Index_II.py
+++ 198_Permutation_Index_II.py
@@ -11,9 +11,6 @@ Example:
 """
 
 
-#run out of memory
-
-from itertools import permutations
 
 class Solution:
     """
@@ -66,55 +63,3 @@ a = Solution()
 
 print(a.permutationIndexII([10,10,10,10,9,8,7,6,5,4,3,2,1]))
 
-"""
-
-class Solution:
-    # @param {int[]} A an integer array
-    # @return {long} a long integer
-    def permutationIndexII(self, A):
-        if A is None or len(A) == 0:
-            return 0
-
-        index = 1
-        factor = 1
-        for i in range(len(A) - 1, -1, -1):
-            hash_map = {A[i]: 1}
-            rank = 0
-            for j in range(i + 1, len(A)):
-                if A[j] in hash_map.keys():
-                    hash_map[A[j]] += 1
-                else:
-                    hash_map[A[j]] = 1
-                # get rank
-                if A[i] > A[j]:
-                    rank += 1
-
-            index += rank * factor / self.dupPerm(hash_map)
-            factor *= (len(A) - i)
-
-        return index
-
-
-    def dupPerm(self, hash_map):
-        if hash_map is None or len(hash_map) == 0:
-            return 0
-        dup = 1
-        for val in hash_map.values():
-            dup *= self.factorial(val)
-
-        return dup
-
-
-    def factorial(self, n):
-        r = 1
-        for i in range(1, n + 1):
-            r *= i
-
-        return r
-
-
-a = Solution()
-
-print(a.permutationIndexII([10,10,10,10,9,8,7,6,5,4,3,2,1]))
-
-"""
